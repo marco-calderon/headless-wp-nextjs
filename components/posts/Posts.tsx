e8cf2c340d1b08835f0dbaa236f3bc019e52b19a
Marco Calderon <mcalderon.code@gmail.com>
2022-11-22 22:08:21 -0800

    Using REST API instead of graphql

--- components/posts/Posts.tsx
+++ components/posts/Posts.tsx
@@ -41,22 +41,21 @@ const ANOTHER_QUERY = gql`
 `;
 
 const Posts = () => {
-  // const { posts, isLoading, isError } = usePosts(1);
-  const { data: posts, loading: isLoading, error: isError } = useQuery(ANOTHER_QUERY);
+  const { posts, isLoading, isError } = usePosts(1);
+  // const { data: posts, loading: isLoading } = useQuery(ANOTHER_QUERY);
 
   if (isLoading) {
     return <p>Loading...</p>;
   }
 
-  if (isError) {
-    return <p>{JSON.stringify(isError)}</p>
-  }
-  
-  return <p>{JSON.stringify(posts)}</p>;
-
   return (
     <div className="flex flex-col gap-5">
-     
+      {posts &&
+        posts.map((p) => (
+          <Link key={p.id} href={`/${p.id}`}>
+            <Post post={p} />
+          </Link>
+        ))}
     </div>
   );
 };
